f32116e61c8b9048650ecdd0f11c8a471f0c308e
Lazer98 <96163637+Lazer98@users.noreply.github.com>
2023-06-26 15:16:57 +0300

    Update Contact.vue

--- src/components/deutsch/Contact.vue
+++ src/components/deutsch/Contact.vue
@@ -1,22 +1,51 @@
 <template>
   <v-main>
     <v-row class="justify-center">
-      <a href="https://wa.me/+972584874474">
-        <svg-icon type="mdi" :path="pathWhatsapp" class="ml-10 mr-5 icon"></svg-icon>
-      </a>
-      <a href="https://www.linkedin.com/in/eliezer-tillmann-leiner-29951419a/">
-        <svg-icon type="mdi" :path="pathEmail" class="mr-5 icon" @click="sendEmailViaIcon"></svg-icon>
-      </a>
-      
-        <svg-icon type="mdi" :path="pathPhone" class="mr-5 icon" @click="callViaIcon"></svg-icon>
-  
-      <a href="https://www.instagram.com/lazer_tillmann/">
-        <svg-icon type="mdi" :path="pathInstagram" class="mr-5 icon"></svg-icon>
-      </a>
-      <a href="https://www.linkedin.com/in/eliezer-tillmann-leiner-29951419a/">
-        <svg-icon type="mdi" :path="pathLinkedin" class="mr-5 icon"></svg-icon>
-      </a>
-      
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <a href="https://wa.me/+972584874474">
+            <svg-icon v-bind="props" :color="isHovering ? 'yellow' : '#8b008b'" type="mdi" :path="pathWhatsapp"
+              class="mb-10 mr-5 icon"></svg-icon>
+          </a>
+        </template>
+      </v-hover>
+
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <a href="https://www.linkedin.com/in/eliezer-tillmann-leiner-29951419a/">
+            <svg-icon v-bind="props" :color="isHovering ? 'yellow' : '#8b008b'" type="mdi" :path="pathEmail"
+              class="mb-10 mr-5 icon" @click="sendEmailViaIcon"></svg-icon>
+          </a>
+        </template>
+      </v-hover>
+
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <a href="tel:+972584874474">
+            <svg-icon v-bind="props" :color="isHovering ? 'yellow' : '#8b008b'" type="mdi" :path="pathPhone"
+              class="mb-10 mr-5 icon" @click="callViaIcon"></svg-icon>
+          </a>
+        </template>
+      </v-hover>
+
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <a href="https://www.instagram.com/lazer_tillmann/">
+            <svg-icon v-bind="props" :color="isHovering ? 'yellow' : '#8b008b'" type="mdi" :path="pathInstagram"
+              class="mb-10 mr-5 icon"></svg-icon>
+          </a>
+        </template>
+      </v-hover>
+
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <a href="https://www.linkedin.com/in/eliezer-tillmann-leiner-29951419a/">
+            <svg-icon v-bind="props" :color="isHovering ? 'yellow' : '#8b008b'" type="mdi" :path="pathLinkedin"
+              class="mb-10 mr-5 icon"></svg-icon>
+          </a>
+        </template>
+      </v-hover>
+
     </v-row>
     <v-sheet width="600" class="mx-auto mt-5" color="transparent">
 
@@ -68,7 +97,7 @@ export default {
     pathWhatsapp: mdiWhatsapp,
     pathEmail: mdiAt,
     pathInstagram: mdiInstagram,
-    pathPhone: mdiPhoneOutline ,
+    pathPhone: mdiPhoneOutline,
 
   }),
   components: {
@@ -107,9 +136,11 @@ export default {
 <style> .white-text {
    color: white;
  }
+
  .icon {
-  width: 32px; /* Desired width */
-  height: 32px; /* Desired height */
-}
-</style>
+   width: 32px;
+   /* Desired width */
+   height: 32px;
+   /* Desired height */
+ }</style>
   
